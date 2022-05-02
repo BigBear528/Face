ceafbdcff2862ba9c81a58738f64c830b081df9d
BigBear528 <BigBear528@163.com>
2022-05-02 22:41:18 +0800

    jwt

--- src/main/java/com/face/config/CorsConfig.java
+++ src/main/java/com/face/config/CorsConfig.java
@@ -9,8 +9,8 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    // 当前跨域请求最大有效时长。这里默认1天
-    private static final long MAX_AGE = 24 * 60 * 60;
+    // 当前跨域请求最大有效时长。这里默认7天
+    private static final long MAX_AGE = 24 * 60 * 60 * 7;
 
     @Bean
     public CorsFilter corsFilter() {
